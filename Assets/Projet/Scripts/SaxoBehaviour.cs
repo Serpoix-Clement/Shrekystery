5f871862d791221ef3fc545b86c6b5a5cf0d3b66
Lurmega <c.serpoix@rubika-edu.re>
2025-04-09 21:08:17 +0400

    fin terminé finito

--- Assets/Projet/Scripts/SaxoBehaviour.cs
+++ Assets/Projet/Scripts/SaxoBehaviour.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaxoBehaviour : MonoBehaviour
 {
     public Canvas canvasE;
+    public GameObject canvasEGO;
+    public Canvas canvasDialogue;
     private bool dansLaZone;
+    public List<GameObject> listDialogue;
+    public Queue<GameObject> fileDialogue;
+    private GameObject dialogueActu;
 
     private void Start()
     {
         dansLaZone = false;
+        fileDialogue = new Queue<GameObject>();
+
+        foreach(GameObject GO in listDialogue)
+        {
+            fileDialogue.Enqueue(GO);
+        }
     }
 
     private void Update()
@@ -18,7 +30,13 @@ public class SaxoBehaviour : MonoBehaviour
         {
             if(Input.GetKeyDown(KeyCode.E))
             {
-                Debug.Log("win : saxophone trouvé");
+                canvasEGO.SetActive(false);
+                canvasDialogue.enabled = true;
+                for(int i = 0; i < 5; i++)
+                {
+                    StartCoroutine(waitForRealEnd());
+                    StartCoroutine(waitForEnd(i));
+                }
             }
         }
     }
@@ -33,5 +51,25 @@ public class SaxoBehaviour : MonoBehaviour
     {
         dansLaZone = false;
         canvasE.enabled = false;
+    }
+
+    public IEnumerator waitForEnd(int i)
+    {
+        yield return new WaitForSeconds(2*i);
+        dialogueActu = fileDialogue.Dequeue();
+        dialogueActu.SetActive(true);
+        StartCoroutine(waitForNext());
+    }
+
+    public IEnumerator waitForNext()
+    {
+        yield return new WaitForSeconds(1.75f);
+        dialogueActu.SetActive(false);
+    }
+
+    public IEnumerator waitForRealEnd()
+    {
+        yield return new WaitForSeconds(9.5f);
+        SceneManager.LoadScene(1);
     }
 }
