8cc8c5937c32fa27cf2496708bced4f7e3511443
Serpoix-Clement <c.serpoix@rubika-edu.re>
2025-04-09 17:22:05 +0400

    Particules du glaçon

--- Assets/Projet/Scripts/FireScript.cs
+++ Assets/Projet/Scripts/FireScript.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class FireScript : MonoBehaviour
 {
+    public ParticleSystem PS;
+
     private void OnTriggerEnter(Collider other)
     {
         if (PlayerController.Instance.glacon)
         {
-            PlayerController.Instance.glacon = false;
-            PlayerController.Instance.cleFinal = true;
+            PS.Stop();
+            PS.Play();
+            var main = PS.main;
+            main.startSpeed = 5f;
+            StartCoroutine(compteur());
         }
+    }
+
+    public IEnumerator compteur()
+    {
+        yield return new WaitForSeconds(5f);
+        PlayerController.Instance.glacon = false;
+        PlayerController.Instance.cleFinal = true;   
     }
 }
